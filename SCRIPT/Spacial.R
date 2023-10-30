adcc0fd17c7df3992b1a0098660839587465e5ab
Luis-Borda <142829063+Luis-Borda@users.noreply.github.com>
2023-10-29 22:30:04 -0500

    Update Spacial.R

--- SCRIPT/Spacial.R
+++ SCRIPT/Spacial.R
@@ -31,6 +31,187 @@ bogota<-opq(bbox = getbb("Bogotá Colombia"))
 bogota
 
 ################################################################################
+####              CREAR VARIABLES                                           ####
+################################################################################
+
+
+#------------------------------------------------------------------------------#
+
+# Se crea la variable "parques"
+
+# Se extrae la información de todos los parques de Bogotá
+
+parques <- opq(bbox = getbb("Bogotá Colombia")) %>%
+  add_osm_feature(key = "leisure" , value = "park") 
+
+# Se cambia el formato para que sea un objeto sf (simple features)
+
+parques_sf <- osmdata_sf(parques)
+
+# De las features del parque, interesa su geomoetría y donde estan ubicados 
+
+parques_geometria <- parques_sf$osm_polygons %>% 
+  select(osm_id, name)
+
+# Calculamos el centroide de cada parque para aproximar su ubciacion como un solo punto 
+
+centroides <- gCentroid(as(parques_geometria$geometry, "Spatial"), byid = T)
+
+# Creamos el mapa de Bogotá 
+
+leaflet() %>%
+  addTiles() %>%
+  setView(lng = longitud_central, lat = latitud_central, zoom = 12) %>%
+  addPolygons(data = parques_geometria, col = "red",weight = 10,
+              opacity = 0.8, popup = parques_geometria$name) %>%
+  addCircles(lng = centroides$x, 
+             lat = centroides$y, 
+             col = "darkblue", opacity = 1, radius = 1)
+
+# Ahora se calcula la distancia de cada apartamento al centroide de cada parque
+# Primero, se toman los datos geoespaciales y se convierten al formato sf (simple features)
+# Esto para que esten en el mismo formato de los parques y poder sacar distancia. 
+
+db_sf <- st_as_sf(train, coords = c("lon", "lat"))
+
+# Especificamos el sistema de coordenadas.
+
+st_crs(db_sf) <- 4326
+
+# convertimos los scontroides a formato sf(simple features)
+
+centroides_sf <- st_as_sf(centroides, coords = c("x", "y"))
+
+
+# Calculamos las distancias para cada combinacion immueble - parque
+
+dist_matrix <- st_distance(x = db_sf, y = centroides_sf)
+
+# Encontramos la distancia mínima a un parque
+
+dist_min_parque <- apply(dist_matrix, 1, min)
+
+# La agregamos como variablea nuestra base de datos original # *****************
+
+train <- train %>% mutate(distancia_parque = dist_min_parque)
+
+p <- ggplot(train, aes(x = distancia_parque)) +
+  geom_histogram(bins = 50, fill = "gold", alpha = 0.4) +
+  labs(x = "Distancia mínima a un parque en metros", y = "Cantidad",
+       title = "Distribución de la distancia a los parques") +
+  theme_bw()
+ggplotly(p)
+
+#Relación del precio vs la distancia al parque 
+
+p <- ggplot(train%>%sample_n(1000), aes(x = distancia_parque, y = price)) +
+  geom_point(col = "gold2", alpha = 0.4) +
+  labs(x = "Distancia mínima a un parque en metros (log-scale)", 
+       y = "Valor de venta  (log-scale)",
+       title = "Relación entre la proximidad a un parque y el precio del immueble") +
+  scale_x_log10() +
+  scale_y_log10(labels = scales::dollar) +
+  theme_bw()
+ggplotly(p)
+
+# Se evalua si el tamaño del parque más cercano influye #
+
+posicion <- apply(dist_matrix, 1, function(x) which(min(x) == x))
+
+# De la geometria de los parques extraemos el aréa
+
+areas <- st_area(parques_geometria)
+
+#Agregamos la variable a nuestra base de datos original - SE INCLUYE VARIABLE POSICION -
+
+train <- train %>%
+  mutate(area_parque = as.numeric(areas[posicion]))
+
+# Se realiza el gráfico (plot) de la relación 
+
+p <- ggplot(train%>%sample_n(1000), aes(x = area_parque, y = price)) +
+  geom_point(col = "gold2", alpha = 0.4) +
+  labs(x = "Área del parque más cercano (log-scale)", 
+       y = "Valor del arriendo (log-scale)",
+       title = "Relación entre área de un parque y el precio del immueble") +
+  scale_x_log10() +
+  scale_y_log10(labels = scales::dollar) +
+  theme_bw()
+ggplotly(p)
+
+####### *VARIABLE VIAS* (mismo procedimiento) ############
+
+# Extraemos la info de las estaciones del Transmilenio
+
+parada_de_bus <- opq(bbox = getbb("Bogotá Colombia")) %>%
+  add_osm_feature(key ='amenity' , value = 'bus_station') 
+
+# Cambiamos el formato para que sea un objeto sf (simple features) (mismo proccedimiento anteriormente descrito)
+
+parada_de_bus_sf <- osmdata_sf(parada_de_bus)
+
+# De las features de estaciones nos interesa su geomoetría y donde están ubicados 
+
+parada_de_bus_sf_geometria <- parada_de_bus_sf$osm_polygons %>% 
+  select(osm_id, name)
+
+# Calculamos el centroide de cada estacione para aproximar su ubicación como un solo punto 
+
+centroides <- gCentroid(as(parada_de_bus_sf_geometria$geometry, "Spatial"), byid = T)
+#** aquí tenias este código en comentario*#
+
+centroides <-st_centroid(parada_de_bus_sf_geometria$geometry)
+
+# Creamos el mapa de Bogotá  con los paraderos
+
+leaflet() %>%
+  addTiles() %>%
+  setView(lng = longitud_central, lat = latitud_central, zoom = 12) %>%
+  addPolygons(data = parada_de_bus_sf_geometria, col = "red",weight = 10,
+              opacity = 0.8, popup = parada_de_bus_sf_geometria$name) %>%
+  addCircles(data=centroides,col = 'blue' , opacity = 0.5, radius = 1)
+
+#lng = centroides$x, Por qué no pusimos esto?
+#            lat = centroides$y, 
+#            col = '#698B69' , opacity = 0.5, radius = 1)
+
+# Primero tomamos nuestros datos y los convertimos al formato sf (simple features)
+# Esto para que esten en el mismo formato de los paraderos y poder calcuar distancias. 
+
+train_sf <- st_as_sf(train, coords = c("lon", "lat"), crs=4326)
+
+# convertimos los centroides a formato sf(simple features) 
+
+centroides_sf <- st_as_sf(centroides, coords = c("lon", "lat"), crs=4326)
+#** aquí tenias este código en comentario* x2 ¿Why?#
+
+centroides_sf <- do.call(rbind, st_geometry(centroides)) %>% 
+  as_tibble() %>% setNames(c("lon","lat")) 
+centroides_sf <- st_as_sf(centroides_sf, coords = c("lon", "lat"), crs=4326)
+
+
+# Calculamos las distancias al paradero mas cercano
+
+nearest <- st_nearest_feature(db_sf,centroides_sf)
+
+train<- train %>% mutate(distancia_bus=st_distance(x = db_sf, y = centroides_sf[nearest,], by_element=TRUE))
+
+
+p <- ggplot(train, aes(x = distancia_bus)) +
+  geom_histogram(bins = 50, fill = "darkblue", alpha = 0.4) +
+  labs(x = "Distancia mínima a un parad de bus en metros", y = "Cantidad",
+       title = "Distribución de la distancia a las parada de bus") +
+  theme_bw()
+library(units)
+ggplotly(p)
+
+
+
+
+
+
+
+################################################################################
 ####              Obtenenemos las universidades                             ####
 ################################################################################
 
